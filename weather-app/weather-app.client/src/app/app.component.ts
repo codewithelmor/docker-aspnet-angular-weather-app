69d983458e63a87d56e78722e240fb7978bee197
Elmor Cabalfin <cabalfinelmor17@gmail.com>
2023-11-26 00:46:16 +0800

    Updated client base url

--- weather-app/weather-app.client/src/app/app.component.ts
+++ weather-app/weather-app.client/src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
   }
 
   getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+    this.http.get<WeatherForecast[]>('http://localhost:5032/weatherforecast').subscribe(
       (result) => {
         this.forecasts = result;
       },
